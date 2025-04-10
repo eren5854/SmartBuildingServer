f4ad079b040e0e4d94e22a3c84b595446c45d7f9
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-11 02:18:04 +0300

    GetRoom yapısı kuruldu

---
+++ SmartBuildingServer.Application/Features/Rooms/GetRoom/GetRoomCommand.cs
@@ -0,0 +1,6 @@
+﻿using ED.Result;
+using MediatR;
+using SmartBuildingServer.Domain.Rooms;
+
+namespace SmartBuildingServer.Application.Features.Rooms.GetRoom;
+public sealed record GetRoomCommand(Guid Id) : IRequest<Result<Room>>;
---
+++ SmartBuildingServer.Application/Features/Rooms/GetRoom/GetRoomCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using ED.Result;
+using MediatR;
+using SmartBuildingServer.Domain.Repositories;
+using SmartBuildingServer.Domain.Rooms;
+
+namespace SmartBuildingServer.Application.Features.Rooms.GetRoom;
+internal sealed class GetRoomCommandHandler(
+    IRoomRepository roomRepository) : IRequestHandler<GetRoomCommand, Result<Room>>
+{
+    public async Task<Result<Room>> Handle(GetRoomCommand request, CancellationToken cancellationToken)
+    {
+        Room room = await roomRepository.GetByExpressionAsync(g => g.Id == request.Id, cancellationToken);
+        if (room is null)
+        {
+            return Result<Room>.Failure($"Room with id {request.Id} not found.");
+        }
+
+        return Result<Room>.Succeed(room);
+    }
+}
