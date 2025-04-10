9c327d90fc37ed9f42d53be0614939cb1b6855b9
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-11 01:27:05 +0300

    şifre değiştirme eklendi

--- SmartBuildingServer.Application/Features/Auth/ChangePassword/ChangePasswordCommandHandler.cs
+++ SmartBuildingServer.Application/Features/Auth/ChangePassword/ChangePasswordCommandHandler.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ED.Result;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using SmartBuildingServer.Domain.Users;
 
 namespace SmartBuildingServer.Application.Features.Auth.ChangePassword;
-internal class ChangePasswordCommandHandler
+internal sealed class ChangePasswordCommandHandler(
+    UserManager<AppUser> userManager) : IRequestHandler<ChangePasswordCommand, Result<string>>
 {
+    public async Task<Result<string>> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+    {
+        AppUser? user = await userManager.FindByIdAsync(request.AppUserId.ToString());
+        if (user is null)
+        {
+            return Result<string>.Failure("User not found");
+        }
+
+        if(request.CurrentPassword == request.NewPassword)
+        {
+            return Result<string>.Failure("Yeni şifre mevcut şifreden farklı olmalı");
+        }
+
+        IdentityResult result = await userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+        if (!result.Succeeded)
+        {
+            return Result<string>.Failure("Hata!! Şifre değiştirme başarısız!!");
+        }
+
+        return Result<string>.Succeed("Şifre değiştirme başarılı.");
+    }
 }
