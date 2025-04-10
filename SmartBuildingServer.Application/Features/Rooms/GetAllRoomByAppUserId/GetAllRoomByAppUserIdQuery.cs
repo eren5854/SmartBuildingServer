a36a56bc453b575a7cef4a228d8e2f353ca2eb31
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-11 02:12:42 +0300

    GetAllRoomByAppUserId yapısı kuruldu

---
+++ SmartBuildingServer.Application/Features/Rooms/GetAllRoomByAppUserId/GetAllRoomByAppUserIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using ED.Result;
+using MediatR;
+using SmartBuildingServer.Domain.Rooms;
+
+namespace SmartBuildingServer.Application.Features.Rooms.GetAllRoomByAppUserId;
+public sealed record GetAllRoomByAppUserIdQuery(Guid AppUserId) : IRequest<Result<List<Room>>>;
---
+++ SmartBuildingServer.Application/Features/Rooms/GetAllRoomByAppUserId/GetAllRoomByAppUserIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using ED.Result;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SmartBuildingServer.Domain.Repositories;
+using SmartBuildingServer.Domain.Rooms;
+
+namespace SmartBuildingServer.Application.Features.Rooms.GetAllRoomByAppUserId;
+internal sealed class GetAllRoomByAppUserIdQueryHandler(
+    IRoomRepository roomRepository) : IRequestHandler<GetAllRoomByAppUserIdQuery, Result<List<Room>>>
+{
+    public async Task<Result<List<Room>>> Handle(GetAllRoomByAppUserIdQuery request, CancellationToken cancellationToken)
+    {
+        List<Room> rooms = await roomRepository.GetAll().Where(g => g.AppUserId == request.AppUserId).OrderBy(o => o.CreatedAt)
+            .ToListAsync(cancellationToken);
+
+        if (rooms is null || rooms.Count == 0)
+        {
+            return Result<List<Room>>.Failure("Hiç oda bulunamadı");
+        }
+
+        return Result<List<Room>>.Succeed(rooms);
+    }
+}
