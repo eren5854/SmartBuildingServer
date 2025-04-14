eb1aeac4a947155620d0bed81266a403c5dc2369
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-15 02:12:00 +0300

    Delete Sensor data yapısı kuruldu

---
+++ SmartBuildingServer.Application/Features/SensorDatas/DeleteSensorData/DeleteSensorDataCommand.cs
@@ -0,0 +1,5 @@
+﻿using ED.Result;
+using MediatR;
+
+namespace SmartBuildingServer.Application.Features.SensorDatas.DeleteSensorData;
+public sealed record DeleteSensorDataCommand(Guid Id) : IRequest<Result<string>>;
---
+++ SmartBuildingServer.Application/Features/SensorDatas/DeleteSensorData/DeleteSensorDataCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using ED.GenericRepository;
+using ED.Result;
+using MediatR;
+using SmartBuildingServer.Domain.Repositories;
+using SmartBuildingServer.Domain.Sensors;
+
+namespace SmartBuildingServer.Application.Features.SensorDatas.DeleteSensorData;
+internal sealed class DeleteSensorDataCommandHandler(
+    ISensorDataRepository sensorDataRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteSensorDataCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteSensorDataCommand request, CancellationToken cancellationToken)
+    {
+        SensorData sensorData = await sensorDataRepository.GetByExpressionAsync(g => g.Id == request.Id, cancellationToken);
+        if (sensorData is null)
+        {
+            return Result<string>.Failure("Sensor bulunamadı!");
+        }
+        
+        sensorDataRepository.Delete(sensorData);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Result<string>.Succeed("Sensor silindi!");
+    }
+}
