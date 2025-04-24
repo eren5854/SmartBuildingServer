e09f08a30901cc81b87b1a18c993b4b941407b5b
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-24 13:44:21 +0300

    update sensor data yapısı kuruldu

---
+++ SmartBuildingServer.Application/Features/SensorDatas/UpdateSensorData/UpdateSensorDataCommand.cs
@@ -0,0 +1,10 @@
+﻿using ED.Result;
+using MediatR;
+using SmartBuildingServer.Domain.Enums;
+
+namespace SmartBuildingServer.Application.Features.SensorDatas.UpdateSensorData;
+public sealed record UpdateSensorDataCommand(
+    Guid Id,
+    SensorTypeSmartEnum SensorType,
+    string DataName,
+    int PinNumber) : IRequest<Result<string>>;
