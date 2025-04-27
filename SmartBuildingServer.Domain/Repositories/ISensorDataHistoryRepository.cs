2bcb82f3c124319e805d53fd3f07d0bab8ea0183
eren5854 <ihsanerendelibas@hotmail.com>
2025-04-27 23:26:06 +0300

    .

---
+++ SmartBuildingServer.Domain/Repositories/ISensorDataHistoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using ED.GenericRepository;
+using SmartBuildingServer.Domain.Logs;
+
+namespace SmartBuildingServer.Domain.Repositories;
+public interface ISensorDataHistoryRepository : IRepository<SensorDataHistory>
+{
+}
---
+++ SmartBuildingServer.Infrastructure/Repositories/SensorDataHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ED.GenericRepository;
+using SmartBuildingServer.Domain.Logs;
+using SmartBuildingServer.Domain.Repositories;
+using SmartBuildingServer.Infrastructure.Context;
+
+namespace SmartBuildingServer.Infrastructure.Repositories;
+public sealed class SensorDataHistoryRepository : Repository<SensorDataHistory, ApplicationDbContext>, ISensorDataHistoryRepository
+{
+    public SensorDataHistoryRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
