ae2d359d8b9d898b99ee9b8b69c1d3f546a6d731
eren5854 <ihsanerendelibas@hotmail.com>
2025-05-06 03:48:42 +0300

    .

---
+++ SmartBuildingServer.Domain/Histories/SensorDataHistory.cs
@@ -0,0 +1,14 @@
+﻿using SmartBuildingServer.Domain.Entities;
+using SmartBuildingServer.Domain.Sensors;
+using System.Text.Json.Serialization;
+
+namespace SmartBuildingServer.Domain.Logs;
+public sealed class SensorDataHistory : Entity
+{
+    public Guid SensorDataId { get; set; }
+    [JsonIgnore]
+    public SensorData SensorData { get; set; } = default!;
+
+    public double? Value { get; set; }
+    public string? Value2 { get; set; }
+}
